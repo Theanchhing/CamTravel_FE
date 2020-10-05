7cd07125767e26cc5a03084bc1ddf42f6ea104cf
PhanSovanna <phansovanna18@kit.edu.kh>
2020-10-05 20:21:55 +0700

    fix error

---
+++ src/components/siemreap/Essential.vue
@@ -0,0 +1,101 @@
+<template>
+  <div style="padding: 50px">
+    <center>
+      <div class="hr">
+        <h3><b>Essential Siem Reap</b></h3>
+      </div>
+      <hr />
+    </center>
+
+    <div class="row">
+      <div class="col s7 push-s5">
+        <span class="flow-text go"
+          ><img
+            src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/14/46/7d/7d/khmer-metal.jpg?w=400&h=-1&s=1"
+            alt="go"
+        /></span>
+        <span class="flow-text go1"
+          ><img
+            src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/32/eb/53/bayon-temple.jpg?w=400&h=-1&s=1"
+            alt="go"
+        /></span>
+        <span class="flow-text go2"
+          ><img
+            src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0c/be/41/21/20160827-163259-01-largejpg.jpg?w=500&h=300&s=1"
+            alt="go"
+        /></span>
+      </div>
+      <div class="col s5 pull-s7">
+        <span class="flow-text"
+          ><h4><b>Go-Play</b></h4></span
+        >
+        <span class="flow-text"
+          ><h6>
+            <b>Places to see, ways to wander, and signature experiences.</b>
+          </h6></span
+        >
+      </div>
+
+      <div class="row">
+        <div class="col s7 push-s5">
+          <span class="flow-text go3"
+            ><img
+              src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/84/39/f3/jaya-house-riverpark.jpg?w=500&h=300&s=1"
+              alt="go"
+          /></span>
+          <span class="flow-text go4"
+            ><img
+              src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/b1/77/c4/viroth-s-hotel.jpg?w=400&h=-1&s=1"
+              alt="go"
+          /></span>
+          <span class="flow-text go5"
+            ><img
+              src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/dd/59/62/we-loved-the-pool.jpg?w=400&h=300&s=1"
+              alt="go"
+          /></span>
+        </div>
+        <div class="col s5 pull-s7">
+          <span class="flow-text"
+            ><h4><b>Go-Rest</b></h4></span
+          >
+          <span class="flow-text"
+            ><h6>
+              <b>A mix of the charming, modern, and tried and true.</b>
+            </h6></span
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.go img {
+  width: 250px;
+  padding: 10px;
+}
+.go1 img {
+  width: 250px;
+  padding: 10px;
+}
+.go2 img {
+  width: 250px;
+  height: 173px;
+  padding: 10px;
+}
+.go3 img {
+  width: 250px;
+  height: 173px;
+  padding: 10px;
+}
+.go4 img {
+  width: 250px;
+  padding: 10px;
+  height: 173px;
+}
+.go5 img {
+  width: 250px;
+  height: 173px;
+  padding: 10px;
+}
+</style>
